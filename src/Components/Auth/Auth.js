17ce04cbc17ac88071d9aa5f3065bc992ae1ec3e
Carson Davies <carsondavies@Carsons-MBP.T-mobile.com>
2020-10-01 17:22:34 -0600

    routing and auth redux done auth and header components shaping up

--- src/Components/Auth/Auth.js
+++ src/Components/Auth/Auth.js
@@ -1,12 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios';
+import { withRouter } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { loginUser } from '../../dux/userReducer'
 
-const Auth = () => {
+const Auth = (props) => {
+
+  const [state, setState] = useState({
+    email: '',
+    password: ''
+  })
+
+  const { email, password } = state;
+
+  function handleChange(e) {
+    const { name, value } = e.target
+    setState(state => ({ ...state, [name]: value }))
+  }
+
+  function handleLogin(e) {
+    axios
+      .post('auth/login', { email, password })
+      .then((res) => {
+        props.loginUser(res.data)
+        props.history.push('/profile/:id')
+      })
+  }
+
+
+
+
+
 
   return (
-    <div>
-      Auth.js
+    <div className='auth-view'>
+      <div className='auth-container'>
+        <div className='auth-logos'>
+          <h1>
+            logo goes here
+          </h1>
+          <h2>Login/Register here! Or choose one of the options above!</h2>
+        </div>
+        <div className='input-boxes'>
+          <label>Email:
+            <input
+              type='text'
+              name='email'
+              value={email}
+              onChange={handleChange}
+              placeholder='enter email' />
+          </label>
+          <label>Password:
+             <input
+              type='password'
+              name='password'
+              value={password}
+              onChange={handleChange}
+              placeholder='enter password' />
+          </label>
+        </div>
+        <div className='auth-button-container'>
+          <button onClick={() => { handleLogin() }}>Login</button>
+          <button onClick={() => { props.history.push('/registeractor') }}>Register</button>
+        </div>
+        AUTH.JS
+      </div>
     </div>
   )
 }
 
-export default Auth
+const mapStatetoProps = reduxState => reduxState
+
+export default connect(mapStatetoProps, { loginUser })(withRouter(Auth))
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,44 @@
 import React from 'react'
+import { Link, withRouter } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { logoutUser } from '../../dux/userReducer'
+import axios from 'axios'
 
-const Header = () => {
+const Header = (props) => {
+  const logout = () => {
+    axios.delete('auth/logout').then(() => {
+      props.logoutUser()
+    })
+  }
 
   return (
-    <div>
-      Header.js
+    <div className='nav-container'>
+      {props.isLoggedIn === true ?
+        <div className='profile-header'>
+          <Link to='/profile/:id' className='profile-pic'>
+            {/* {props.user.headshot} */}
+            Profile
+        </Link>
+          <button className='logout-button' onClick={() => logout()}>
+            logout
+          </button>
+        </div>
+        : null}
+      <Link to='/auditions'>
+        Auditions
+      </Link>
+      <Link to='/theaters'>
+        Theaters
+      </Link>
+      <Link to='/resourcebrowser'>
+        Resource Browser
+      </Link>
+
+        Header.js
     </div>
   )
 }
 
-export default Header
+const mapStateToProps = reduxState => reduxState
+
+export default connect(mapStateToProps, { logoutUser })(withRouter(Header))
