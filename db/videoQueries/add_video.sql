d505a3a745aa19e8b89f4a3c3da1881c7bb50f98
Carson Davies <carsond11@gmail.com>
2020-11-08 22:50:03 -0700

    added adding video function to controller havent tested yet

---
+++ db/videoQueries/add_video.sql
@@ -0,0 +1,4 @@
+INSERT INTO videos
+(vocal_range, video_url, video_title, thumbnail)
+VALUES
+($1, $2, $3, $4)
--- server/Controllers/resourceController.js
+++ server/Controllers/resourceController.js
@@ -1,4 +1,34 @@
+const YouTubeGetID = (url) => {
+  var ID = '';
+  url = url.replace(/(>|<)/gi, '').split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/);
+  if (url[2] !== undefined) {
+    ID = url[2].split(/[^0-9a-z_\-]/i);
+    ID = ID[0];
+  }
+  else {
+    ID = url;
+  }
+  return ID;
+}
+
 module.exports = {
+  addVideo: async (req, res) => {
+    const db = req.app.get('db')
+    const {video_url, video_title, vocal_range} = req.params
+
+    const videoId = YouTubeGetID(video_url)
+
+    const [video] = await db.videoQueries.check_video([video_url])
+
+    if (video) {
+      return res.status(409).send('Video already in database')
+    } else {
+      await db.videoQueries.add_video([vocal_range, video_url, video_title, `https://img.youtube.com/vi/${videoId}/default.jpg`])
+      res.status(200).send('Video added to Resource Browser. Thank you for your contribution!')
+    }
+
+
+  },
   addVideoToBass: async (req, res) => {
     //will be an input field for users to paste link to add video to db.
     const db = req.app.get('db')
